57c3f2d2cf476c1494ff811eb42cf503f6a0e640
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-04-20 22:08:44 -0700

    Added variables for employees

--- mysql/employeeSeeds.sql
+++ mysql/employeeSeeds.sql
@@ -1,31 +1,4 @@
-DROP DATABASE IF EXISTS EmployeeCheckDB;
-CREATE DATABASE EmployeeCheckDB;
-
-USE EmployeeCheckDB;
-
-
-create table department (
-    id  INTEGER PRIMARY KEY auto_increment,
-    name VARCHAR(100) not null
-);
-
-create table  roles (
-    id INTEGER PRIMARY KEY auto_increment,
-    title VARCHAR(100) not null,
-    salary DECIMAL not null,
-    department_id INTEGER NOT NULL,
-    foreign key(department_id) references department(id)
-);
-
-create table employee (
-    id INTEGER PRIMARY KEY auto_increment,
-    first_name VARCHAR(50) not null,
-    last_name VARCHAR(50) not null,
-    role_id INTEGER NOT NULL,
-    foreign key(role_id) references roles(id),
-    manager_id INTEGER,
-    foreign key(manager_id) references employee(id)
-);
+use EmployeeCheckDB;
 
 insert into department (name) 
 values 
@@ -43,5 +16,14 @@ values
 ("Mailroom Clerk", "1000", 4),
 ("Researcher", "50000", 3);
 
-select * from department;
-select * from roles;
+insert into 
+employee (first_name, last_name, role_id, manager_id)
+values
+("James", "Franco", 2, 1),
+("Brett", "Favre", 1, 1),
+("Tay", "Diggs", 4, 1),
+("Tom", "Holland", 3, 2),
+("Tyler", "Perry", 3, 2),
+("Channing", "Tatum", 3, 2),
+("Joe", "Rogan", 2, 2);
+
