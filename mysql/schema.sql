97a4f55ff10e150d9bd9354bfa92c025894f7c5d
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-04-20 22:08:19 -0700

    Added schema information

---
+++ mysql/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS EmployeeCheckDB;
+CREATE DATABASE EmployeeCheckDB;
+
+USE EmployeeCheckDB;
+
+
+create table department (
+    id  INTEGER PRIMARY KEY auto_increment,
+    name VARCHAR(100) not null
+);
+
+create table  roles (
+    id INTEGER PRIMARY KEY auto_increment,
+    title VARCHAR(100) not null,
+    salary DECIMAL not null,
+    department_id INTEGER NOT NULL,
+    foreign key(department_id) references department(id)
+);
+
+create table employee (
+    id INTEGER PRIMARY KEY auto_increment,
+    first_name VARCHAR(50) not null,
+    last_name VARCHAR(50) not null,
+    role_id INTEGER NOT NULL,
+    foreign key(role_id) references roles(id),
+    manager_id INTEGER,
+    foreign key(manager_id) references employee(id)
+);
